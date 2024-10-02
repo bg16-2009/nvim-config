7a50b3498e8f5ea18b346321833a4b7a299e9c2c
Bogdan Gabor <bogdan.gabor.cj@gmail.com>
2024-10-02 18:02:59 +0300

    Switch to folke zen-mode

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,62 +1,60 @@
 return {
-  'pocco81/true-zen.nvim',
-  opts = {
-    minimalist = {
-      options = { -- options to be disabled when entering Minimalist mode
-        number = false,
-        relativenumber = false,
-        showtabline = 1,
-        signcolumn = 'no',
-        statusline = '',
-        cmdheight = 0,
-        laststatus = 0,
-        showcmd = false,
-        showmode = false,
-        ruler = false,
-        numberwidth = 1,
-      },
-    },
-    integrations = {
-      tmux = true,
-    },
-  },
-  keys = {
-    {
-      '<leader>zn',
-      '<cmd>TZNarrow<cr>',
-      mode = 'n',
-      desc = 'Narrow focused',
-      noremap = true,
-    },
-    {
-      '<leader>zn',
-      "<cmd>'<,'>TZNarrow<cr>",
-      mode = 'v',
-      desc = 'Ranged narrow focused',
-      noremap = true,
-    },
-    {
-      '<leader>zf',
-      '<cmd>TZFocus<cr>',
-      mode = 'n',
-      desc = 'Focued mode',
-      noremap = true,
-    },
-    {
-      '<leader>zm',
-      '<cmd>TZMinimalist<cr>',
-      mode = 'n',
-      desc = 'Minimalist mode',
-      noremap = true,
-    },
-    {
-      '<leader>za',
-      '<cmd>TZAtaraxis<cr>',
-      mode = 'n',
-      desc = 'Ataraxis mode',
-      noremap = true,
-    },
-  },
+	"folke/zen-mode.nvim",
+	opts = {
+		window = {
+			backdrop = 0.95,
+			width = 1,
+			height = 1,
+			options = {
+				-- signcolumn = "no", -- disable signcolumn
+				-- number = false, -- disable number column
+				-- relativenumber = false, -- disable relative numbers
+				-- cursorline = false, -- disable cursorline
+				-- cursorcolumn = false, -- disable cursor column
+				-- foldcolumn = "0", -- disable fold column
+				-- list = false, -- disable whitespace characters
+			},
+		},
+		plugins = {
+			options = {
+				enabled = true,
+				ruler = false,
+				showcmd = false,
+				laststatus = 0,
+			},
+			twilight = { enabled = false },
+			gitsigns = { enabled = false },
+			tmux = { enabled = true },
+			todo = { enabled = false },
+			kitty = {
+				enabled = false,
+				-- font = "+4",
+			},
+			alacritty = {
+				enabled = false,
+				-- font = "14",
+			},
+			wezterm = {
+				enabled = false,
+				-- font = "+4",
+			},
+			neovide = {
+				enabled = false,
+				-- scale = 1.2,
+				disable_animations = {
+					neovide_animation_length = 0,
+					neovide_cursor_animate_command_line = false,
+					neovide_scroll_animation_length = 0,
+					neovide_position_animation_length = 0,
+					neovide_cursor_animation_length = 0,
+					neovide_cursor_vfx_mode = "",
+				},
+			},
+		},
+		on_open = function(win) end,
+		on_close = function() end,
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>zn", "<Cmd>ZenMode<CR>")
+	end,
 }
-
--- vim: ts=2 sts=2 sw=2 noet
